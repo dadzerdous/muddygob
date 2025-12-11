99ffdda8dc404ea23b9dc0848bcc064a19d31149
dadzerdous <dadzaverous@gmail.com>
2025-12-11 06:13:43 -0500

    Update helps.js

--- commands/helps.js
+++ commands/helps.js
@@ -1,1 +1,12 @@
 
+module.exports = {
+    name: "help",
+    description: "Show available commands.",
+
+    execute(socket) {
+        socket.send(JSON.stringify({
+            type: "system",
+            msg: "Help system is active."
+        }));
+    }
+};
